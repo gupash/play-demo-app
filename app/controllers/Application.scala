7469335c6b455ed1a1d955620e8e5921b799d5ef
ashish <gupta.ashish1997@gmail.com>
2016-05-06 19:58:19 +0530

    First-Play-Demo
    Committed Initial Version of First play demo with User Form

---
+++ app/controllers/Application.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject.Singleton
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.mvc.{Action, Controller}
+
+@Singleton
+class Application extends Controller {
+
+  val userForm = Form(
+    tuple(
+      "name" -> text.verifying(nonEmpty),
+      "age" -> number.verifying(min(18), max(30))
+    )
+  )
+
+  def index = Action {
+    Ok(views.html.index())
+  }
+
+  def submit = Action {
+    implicit request =>
+      val (name, age) = userForm.bindFromRequest.get
+      Ok(views.html.display(s"Hi $name $age !"))
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := """slim-play-scala"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  specs2 % Test
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+resolvers += Resolver.sonatypeRepo("releases")
+
+routesGenerator := InjectedRoutesGenerator
+
+fork in run := true
